9b2ab1b68a6be82d517e3bc4e4f0a494a520ea43
jeff <jeff@debian.home>
2017-12-29 02:03:44 +0100

    First One

---
+++ SampleProj/src/SamplePack/SampleClass.java
@@ -0,0 +1,15 @@
+package SamplePack;
+
+import javax.swing.JOptionPane;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		   JOptionPane.showMessageDialog(null, "Yo man", "InfoBox: This is a Title bar", JOptionPane.INFORMATION_MESSAGE);
+    
+
+	}
+
+}
